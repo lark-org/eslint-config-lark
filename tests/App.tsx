aa015e49d4ae150bc0ab039af53edc99644474d7
guoshuaibing <guoshuaibing452@hellobike.com>
2021-08-29 12:03:09 +0800

    first commit

---
+++ tests/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import { style } from 'emotion'
+
+interface Props {
+  name: string
+}
+
+const Foo = ({ name }: Props) => {
+  onClick()
+
+  let num = 123
+  const [count, setCount] = useState()
+
+  useEffect(() => {
+    const num = 456
+    count++
+  }, [])
+
+  const onClick = () => {
+    setCount({})
+  }
+
+  return (
+    <div css={style(false)} onClick={onClick}>
+      I'm {name}
+    </div>
+  )
+}
+
+export default App
+
+const App = () => <Foo name={123} />
